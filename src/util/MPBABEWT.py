f025fd2dfb0f69820fc593794470378a286525b7
dmdaksh <dmdaksh.97@gmail.com>
2021-07-14 20:34:15 +0000

    optimized MPBABEWT code (using numpy istead of python list)

--- src/util/MPBABEWT.py
+++ src/util/MPBABEWT.py
@@ -8,32 +8,30 @@ def ewt_beta(x):
     elif x > 1:
         bm = 1
     else:
-        bm = (x**4) * (35-84*x+70*(x**2)-20*(x**3))    
+        bm = (x**4) * (35.-84.*x+70.*(x**2)-20.*(x**3))    
     return bm
 
 
 def ewt_meyer_scaling(w1, gamma, n):
-    try:
-        mi = np.math.floor(n/2)
-        w = np.fft.fftshift(np.arange(0, 2*PI, 2*PI/n))
-        w[:mi] -= 2*PI
+    mi = np.math.floor(n/2)
+    w = np.fft.fftshift(np.linspace(0, 2*PI - 2*PI/n, n))
+    w[:mi] -= 2*PI
 
-        aw = np.abs(w)
-        yms = np.zeros((n, 1))
+    aw = np.abs(w)
+    yms = np.zeros(n)
 
-        an = 1/(2*gamma*w1)
-        pbn = (1+gamma)*w1
-        mbn = (1-gamma)*w1
+    an = 1/(2*gamma*w1)
+    pbn = (1+gamma)*w1
+    mbn = (1-gamma)*w1
 
-        for i in range(n):
-            if aw[i] <= mbn:
-                yms[i] = 1
-            elif aw[i] >= mbn and aw[i] <= pbn:
-                yms[i] = np.math.cos(PI*ewt_beta(an*(aw[i]-mbn))/2)
+    for i in range(n):
+        if aw[i] <= mbn:
+            yms[i] = 1
+        elif aw[i] >= mbn and aw[i] <= pbn:
+            yms[i] = np.math.cos(PI*ewt_beta(an*(aw[i]-mbn))/2)
 
-        yms = np.fft.ifftshift(yms)
-    except Exception as e:
-        print(e)
+    yms = np.fft.ifftshift(yms)
+
     return yms
 
 
@@ -43,7 +41,7 @@ def ewt_meyer_wavelet(wn, wm, gamma, n):
     w[:mi] -= 2*PI
 
     aw = np.abs(w)
-    ymw = np.zeros((n, 1))
+    ymw = np.zeros(n)
 
     an = 1/(2*gamma*wn)
     am = 1/(2*gamma*wm)
@@ -65,7 +63,7 @@ def ewt_meyer_wavelet(wn, wm, gamma, n):
 
 
 def ewt_meyer_filter_bank(boundaries, n):
-    len_boundaries = len(boundaries)
+    len_boundaries = boundaries.shape[0]
 
     gamma = 1
     for i in range(len_boundaries-1):
@@ -79,29 +77,31 @@ def ewt_meyer_filter_bank(boundaries, n):
 
     gamma *= (1 - 1/n)
 
-    mfb = [[None] for _ in range(len_boundaries+1)] # np.zeros((len_boundaries+1, 1), dtype = np.float)
+    mfb = np.zeros((n, len_boundaries+1))
     
-    mfb[0] = ewt_meyer_scaling(boundaries[0], gamma, n)
+    mfb[:, 0] = ewt_meyer_scaling(boundaries[0], gamma, n)
 
     for i in range(len_boundaries-1):
-        mfb[i+1] = ewt_meyer_wavelet(boundaries[i], boundaries[i+1], gamma, n)
+        mfb[:, i+1] = ewt_meyer_wavelet(boundaries[i], boundaries[i+1], gamma, n)
     
-    mfb[len_boundaries] = ewt_meyer_wavelet(boundaries[len_boundaries-1], PI, gamma, n)
+    mfb[:, len_boundaries] = ewt_meyer_wavelet(boundaries[len_boundaries-1], PI, gamma, n)
     
     return mfb
 
 def mode_eval_each_channel(zz, mfb):
     ft = np.fft.fft(zz)
-    ewt = [[None] for _ in range(len(mfb))]
-    modes = [[None] for _ in range(len(mfb))]
-    for i in range(len(mfb)):
-        ewt[i] = np.real(np.fft.ifft(np.conjugate(mfb[i]))).ravel()*ft
-        modes[i] = ewt[i].tolist()
-    return modes
+
+    ewt = np.zeros((mfb.shape[1], mfb.shape[0]), dtype='complex_') # ewt.shape = 5*300
+
+    for i in range(mfb.shape[1]):
+        ewt[i] = np.real(np.fft.ifft(np.conjugate(mfb[:, i])))*ft
+
+    return ewt
 
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
+
 data = sio.loadmat('data.mat')['data']
 
 x = data
@@ -115,34 +115,29 @@ combined = np.sum(x, axis = 1)/np.math.sqrt(3)
 
 ff = np.fft.fft(combined)
 
-# plt.plot(np.abs(ff))
-# plt.show()
+frequencies = np.array([5, 10, 20, 30])
 
-
-frequencies = [5, 10, 20, 30]
-
-boundaries = [(frequency * (2*np.math.pi)) / fs for frequency in frequencies]
-# print(boundaries)
+boundaries = np.array([(frequency * (2*PI)) / fs for frequency in frequencies])
+print(boundaries)
 mfb = ewt_meyer_filter_bank(boundaries, len(ff))
+print(mfb.shape)
 
-# bound = 1
+xxx = np.linspace(0, 1, mfb.shape[0])*fs
 
-
-xxx = (np.linspace(0, 1, round(len(mfb[0])))*fs).tolist()
-
-for i in mfb:
-    plt.plot(xxx, i)
+legend = []
+for i in range(mfb.shape[1]):
+    plt.plot(xxx, mfb[:, i])
+    legend.append(i)
 
 plt.xlim(0, round(173.61/2))
 plt.ylim(0,2)
 plt.title('Projection based multivariate EWT filter bank')
+plt.legend(legend)
 plt.show()
 
-modal = [None] * 3
+modal = np.zeros((mfb.shape[1], mfb.shape[0], 3), dtype='complex_')
 
-# print(type(mfb), type(mfb[0]))
 for i in range(3):
-    modal[i] = mode_eval_each_channel(x[:, i], mfb)
+    modal[:, :, i] = mode_eval_each_channel(x[:, i], mfb)
 
-modal = np.array(modal).transpose(1,2,0)
 print(modal.shape)
